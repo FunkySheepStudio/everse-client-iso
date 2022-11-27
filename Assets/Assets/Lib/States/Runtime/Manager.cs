2b58e80bd746c92d30de09de16d93b4528982e48
BartKnucle <sjarde@yahoo.fr>
2022-11-27 16:35:05 +0100

    Add Materials

---
+++ Assets/Assets/Lib/States/Runtime/Manager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace FunkySheep.States
+{
+    [AddComponentMenu("FunkySheep/States/Manager")]
+    public class Manager : FunkySheep.Types.Singleton<Manager>
+    {
+    }
+}
---
+++ Assets/Assets/Lib/States/Runtime/State.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace FunkySheep.States
+{
+    public class State : ScriptableObject
+    {
+    }
+}
+
